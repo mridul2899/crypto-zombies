59b19e172b215acff50a92006bc117df65b64a8f
Mridul Agarwal <mridul2899@gmail.com>
2022-06-03 20:32:30 +0530

    Add Chapter 2 of Lesson 1

---
+++ Lesson 1/Chapter 2/Contract.sol
@@ -0,0 +1,13 @@
+// A contracts is the fundamental building block of Ethereum apps.
+// It stores all the variables and functions.
+
+// 1. Enter solidity version here
+// We'll be able to compile our smart contracts with a compiler
+// version between the ranges specified in the "version pragma".
+pragma solidity >=0.5.0 <0.6.0;
+
+// 2. Create contract here
+// ZombieFactory is the name of the smart contract
+contract ZombieFactory {
+
+}
