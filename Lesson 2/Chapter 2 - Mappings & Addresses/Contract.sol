7aade678598efcd18fc6f3cdd3f58e73fa5e321b
Mridul Agarwal <mridul2899@gmail.com>
2022-06-04 14:23:05 +0530

    Add Lesson 2 Chapter 2

---
+++ Lesson 2/Chapter 2 - Mappings & Addresses/Contract.sol
@@ -0,0 +1,47 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+
+    Zombie[] public zombies;
+
+    // Ethereum blockchain is made of accounts - like bank accounts.
+    // Each account has a balance in Ether, used for sending and receiving payments.
+    // Each account has a unique identifier called address.
+    // Each address is owned by a specific user or smart contract.
+
+    // Mappings allow key-value pairs to be stored and retrieved.
+    // Syntax: mapping (keyType => valueType) modifier variableName;
+    // Example: For a financial app, storing a uint that holds the user's account balance:
+    // mapping (address => uint) public accountBalance;
+
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint256 _dna) private {
+        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
