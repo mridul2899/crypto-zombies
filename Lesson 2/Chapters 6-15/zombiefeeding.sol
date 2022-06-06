5cd4047ea548c600ef589a706a31208b40040dcd
Mridul Agarwal <mridul2899@gmail.com>
2022-06-06 11:31:52 +0530

    Add L2 Chapter 7 - Data location - Storage & Memory

--- Lesson 2/Chapters 6-15/zombiefeeding.sol
+++ Lesson 2/Chapters 6-15/zombiefeeding.sol
@@ -5,4 +5,23 @@ pragma solidity >=0.5.0 <0.6.0;
 // hence making the codebase manageable.
 import "./zombiefactory.sol";
 
-contract ZombieFeeding is ZombieFactory {}
+contract ZombieFeeding is ZombieFactory {
+    // In Solidity, there are two types of locations to store variables - storage and memory.
+    // Storage variables are stored permanently on the blockchain.
+    // Memory variables are temporary, and are erased between external function calls to the contract.
+    // Most times we don't need to use these keywords as Solidity handles these variables automatically.
+    // State variables (variables declared outside of functions) are by default storage.
+    // Variables declared inside functions are memory.
+    // When dealing with reference types, we need to use these keywords within functions.
+
+    // When we change any attribute or object of a storage variable,
+    // it permanently changes the original value on the blockchain because it's a pointer.
+    // However, memory variable is just a copy of the original variable,
+    // and any change will modify only the temporary variable, not the original.
+    // Changing the permanent storage value, in this case, would need explicit value change.
+
+    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        Zombie storage myZombie = zombies[_zombieId];
+    }
+}
