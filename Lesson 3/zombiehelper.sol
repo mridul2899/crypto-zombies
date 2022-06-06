170200244d1c0ed35c52b910e1fcf8bbeb784dd9
Mridul Agarwal <mridul2899@gmail.com>
2022-06-06 20:42:39 +0530

    Add L3 Chapter 9 - Zombie Modifiers

--- Lesson 3/zombiehelper.sol
+++ Lesson 3/zombiehelper.sol
@@ -13,4 +13,25 @@ contract ZombieHelper is ZombieFeeding {
     // This modifier is now used just like functions,
     // after specifying after specifying function's arguments.
     // Example: function name(type arg1) public aboveLevel(1, 2) {}
+
+    // Chapter 9 - Zombie Modifiers
+    // Note: Data location calldata is somehow similar to memory,
+    // but only available to external functions.
+    // TODO: Find more on difference between calldata and memory.
+
+    function changeName(uint256 _zombieId, string calldata _newName)
+        external
+        aboveLevel(2, _zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external
+        aboveLevel(20, _zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
 }
