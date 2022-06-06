ce0ac8ac4687c4a76873de5cbe3c9c7c5de330bb
Mridul Agarwal <mridul2899@gmail.com>
2022-06-06 11:08:21 +0530

    Add L2C5

---
+++ Lesson 2/Chapter 5 - Inheritance/Contract.sol
@@ -0,0 +1,47 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint256 _dna) private {
+        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
+
+// Inheritance can be used for logical inheritance like with subclasses example, Cat is an Animal.
+// Or it can simply be used for organizing code into different and smaller contracts.
+// Inherited contracts also have access to the public functions of the base contract.
+contract ZombieFeeding is ZombieFactory {
+
+}
