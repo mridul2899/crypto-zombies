944c8b177daf25113f1709774e5335f3452cb312
Mridul Agarwal <mridul2899@gmail.com>
2022-06-06 11:13:34 +0530

    Add L2 Chapter 6 - Import
    Since there will be multiple files from here on, instead of having separate chapters, we would rather keep updating these files in different commits. So difference would allow for a better way of seeing what changed between chapters

---
+++ Lesson 2/Chapters 6-15/zombiefeeding.sol
@@ -0,0 +1,8 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+// put import statement here
+// We use import statements to split our codes into different files,
+// hence making the codebase manageable.
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {}
