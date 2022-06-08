64821ca5069aa51ce93508682b586218b0e91118
Mridul Agarwal <mridul2899@gmail.com>
2022-06-08 11:56:16 +0530

    Add L4C9 - Zombie Wins & Losses

--- Lesson 4/zombiefactory.sol
+++ Lesson 4/zombiefactory.sol
@@ -14,6 +14,9 @@ contract ZombieFactory is Ownable {
         uint256 dna;
         uint32 level;
         uint32 readyTime;
+        // Chapter 9 - Zombie Wins & Losses - Part 1
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -22,8 +25,9 @@ contract ZombieFactory is Ownable {
     mapping(address => uint256) ownerZombieCount;
 
     function _createZombie(string memory _name, uint256 _dna) internal {
+        // Chapter 9 - Zombie Wins & Losses - Part 2
         uint256 id = zombies.push(
-            Zombie(_name, _dna, 1, uint32(now + cooldownTime))
+            Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
         ) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
